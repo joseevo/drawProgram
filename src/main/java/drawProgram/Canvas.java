c4b594c744f98f473b2c473cb3d9bc1f3ce42ee3
joseevo <joseevoliveira@gmail.com>
2021-11-22 02:39:37 +0000

    Designed the abstract class Geometry that is going to serve as a bluePrint for the geometric elements sub-classes

--- src/main/java/drawProgram/Canvas.java
+++ src/main/java/drawProgram/Canvas.java
@@ -2,4 +2,28 @@ package drawProgram;
 
 public class Canvas extends Geometry {
 
+	@Override
+	public void checkInput(String[] input) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public char[][] drawX(char[][] arr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public char[][] drawY(char[][] arr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public char[][] draw(String[] input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/main/java/drawProgram/Geometry.java
+++ src/main/java/drawProgram/Geometry.java
@@ -1,5 +1,15 @@
 package drawProgram;
 
+//Abstract class that serves as a blueprint for the geometric elements
 public abstract class Geometry {
+	
+	public abstract void checkInput(String[] input);
+	
+	public abstract char[][] drawX(char[][] arr);
+	
+	public abstract char[][] drawY(char[][] arr);
+		
+	public abstract char[][] draw(String[] input);
+		
 
 }
