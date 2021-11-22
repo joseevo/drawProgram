58cdfcd1c9e8435689bbe70496078578a8286f29
joseevo <joseevoliveira@gmail.com>
2021-11-22 03:17:18 +0000

    Created Sub Class Line and its Unit testing

--- src/test/java/drawProgram/LineTest.java
+++ src/test/java/drawProgram/LineTest.java
@@ -1,14 +1,94 @@
 package drawProgram;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LineTest {
 
+	private Canvas canvas = new Canvas();
+	private String[] inputCanvas = new String[] { "C", "4", "40" };
+	private char[][] arr = canvas.draw(inputCanvas);
+	private Line line = new Line(arr);
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testValid() throws Exception {
+		String[] inputLine = new String[] { "L", "4", "10", "4", "20" };
+		line.draw(inputLine);
 	}
+
+	@Test
+	public void testNoCanvas() {
+		char[][] arr=null;
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			Line line = new Line(arr);
+		});
+		Assert.assertEquals("Please create a Canvas", exception.getMessage());
+	}
+
+	@Test
+	public void testEmpty1() {
+		String[] inputLine = new String[] { "L", "4", "10", "4" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			line.draw(inputLine);
+		});
+		Assert.assertEquals("please insert command, x1, y1, x2, y2", exception.getMessage());
+	}
+
+	@Test
+	public void testNonInt() {
+		String[] inputLine = new String[] { "L", "4", "10", "4", "5.2" };
+		Throwable exception = Assert.assertThrows(NumberFormatException.class, () -> {
+			line.draw(inputLine);
+		});
+		Assert.assertEquals("x1,y1,x2,y2 must be integer numbers", exception.getMessage());
+	}
+
+	@Test
+	public void testOutScope() {
+		String[] inputLine = new String[] { "L", "4", "10", "4", "50" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			line.draw(inputLine);
+		});
+		Assert.assertEquals("Please insert a point inside the canvas scope", exception.getMessage());
+	}
+
+	@Test
+	public void testDiagonal() {
+		String[] inputLine = new String[] { "L", "2", "10", "4", "30" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			line.draw(inputLine);
+		});
+		Assert.assertEquals("please insert an horizontal or vertical line", exception.getMessage());
+	}
+
+	@Test
+	public void testNegative1() {
+		String[] inputLine = new String[] { "L", "-4", "10", "-4", "20" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			line.draw(inputLine);
+		});
+		Assert.assertEquals("x1,y1,x2,y2 must be positive numbers", exception.getMessage());
+	}
+
+	@Test
+	public void testNegative2() {
+
+		String[] inputLine = new String[] { "L", "4", "-10", "4", "-20" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			line.draw(inputLine);
+		});
+		Assert.assertEquals("x1,y1,x2,y2 must be positive numbers", exception.getMessage());
+	}
+	
+	@Test
+	public void testSamePoint() {
+
+		String[] inputLine = new String[] { "L", "4", "10", "4", "10" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			line.draw(inputLine);
+		});
+		Assert.assertEquals("Please insert different point coordinates to create a line", exception.getMessage());
+	}
+
 
 }
