b89b9a72840a0b1d8f26e2f5476b9536cbac72fb
joseevo <joseevoliveira@gmail.com>
2021-11-22 03:30:33 +0000

    Created Class InputReader and its Unit testing

--- src/main/java/drawProgram/InputReader.java
+++ src/main/java/drawProgram/InputReader.java
@@ -1,5 +1,36 @@
 package drawProgram;
 
 public class InputReader {
+	
+	//method that validates if the input isn't empty and uses the first character from the user's input to return the intended subclass
+		public static Geometry action (String[] input, char[][] arr) {
+			
+			if(input.length==0) {
+				throw new IllegalArgumentException ("Please insert an action");
+			}
+			
+			
+			switch (Character.toUpperCase(input[0].charAt(0))) {
+				
+			case 'C':
+				return new Canvas();
+				
+			case 'L':
+				return new Line(arr);
+				
+			case 'R':
+				return new Rectangle(arr);
+				
+			case 'Q':
+				System.out.println("Exit");
+				System.exit(0);
+
+				
+			default:
+				
+				throw new IllegalArgumentException ("Please insert a valid action ([C] - New Canvas, [L] - New Line, [R] - New Rectangle, [Q] - Quit");
+			}
+					
+		}
 
 }
--- src/test/java/drawProgram/InputReaderTest.java
+++ src/test/java/drawProgram/InputReaderTest.java
@@ -1,14 +1,50 @@
 package drawProgram;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class InputReaderTest {
 
+	private char[][] arr = new char[][] { { ' ', 'X' }, { 'X', 'X' }, { 'X', ' ' } };
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testValid1() throws Exception {
+		String[] input = new String[] { "C", "4", "40" };
+		char[][] arr = new char[][] {};
+		InputReader.action(input, arr);
+	}
+
+	@Test
+	public void testValid2() throws Exception {
+		String[] input = new String[] { "L", "1", "1", "1", "3" };
+		InputReader.action(input, arr);
+	}
+
+	@Test
+	public void testValid3() throws Exception {
+		String[] input = new String[] { "R", "1", "1", "4", "4" };
+		InputReader.action(input, arr);
+	}
+
+	@Test
+	public void testNull() {
+		String[] input = new String[] {};
+		char[][] arr = new char[][] {};
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			InputReader.action(input, arr);
+		});
+		Assert.assertEquals("Please insert an action", exception.getMessage());
+
+	}
+	
+	@Test
+	public void testInvalidCommand() {
+		String[] input = new String[] {"P", "1", "1", "4", "4"};
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			InputReader.action(input, arr);
+		});
+		Assert.assertEquals("Please insert a valid action ([C] - New Canvas, [L] - New Line, [R] - New Rectangle, [Q] - Quit", exception.getMessage());
+
 	}
 
 }
