33f25656c163343ae1a1b054aa279c17a18be958
joseevo <joseevoliveira@gmail.com>
2021-11-22 03:06:07 +0000

    Created Sub Class Canvas and its Unit testing

--- src/main/java/drawProgram/Canvas.java
+++ src/main/java/drawProgram/Canvas.java
@@ -2,28 +2,101 @@ package drawProgram;
 
 public class Canvas extends Geometry {
 
+	// maximum size of the number of columns or lines inserted (to prevent an
+	// OutOfMemoryError)
+	private static final int arrayMaxSize = 4000;
+
+	// number of instructions that should be received by the user
+	private int inputSize = 3;
+
+	// global width and height variables
+	private int lines;
+	private int columns;
+
+	// method that validates the user's input
+
 	@Override
 	public void checkInput(String[] input) {
-		// TODO Auto-generated method stub
-		
+		// validates if the input has size 3 (command, width and height)
+		if (input.length != inputSize) {
+
+			throw new IllegalArgumentException("please insert a command, width and height value separated by [space]");
+		}
+
+		// validates if the width and height passed are integer numbers
+		try {
+
+			// declares the variables with the width and height
+			lines = Integer.parseInt(input[1]);
+			columns = Integer.parseInt(input[2]);
+
+		} catch (NumberFormatException ex) {
+
+			throw new NumberFormatException("width and height must be integer numbers");
+
+		}
+
+		// validates if the input does not pass a certain size
+		if (lines > arrayMaxSize && columns > arrayMaxSize) {
+
+			throw new NumberFormatException("The values inserted are too big, please insert smaller values");
+		}
+
+		// validates if the values inserted for the width and height are positive
+		// numbers
+		if (lines <= 0 || columns <= 0) {
+
+			throw new IllegalArgumentException("width and height must be positive numbers");
+		}
+
+		// increments the width and height by 2 for the design of the canvas frame
+		lines += 2;
+		columns += 2;
+
 	}
 
 	@Override
 	public char[][] drawX(char[][] arr) {
-		// TODO Auto-generated method stub
-		return null;
+		char point = '|';
+
+		for (int i = 0; i < lines; i++) {
+
+			arr[i][0] = point;
+			arr[i][columns-1] = point;
+		}
+
+		return arr;
 	}
 
 	@Override
 	public char[][] drawY(char[][] arr) {
-		// TODO Auto-generated method stub
-		return null;
+		char point = '-';
+
+		for (int i = 0; i < columns; i++) {
+
+			arr[0][i] = point;
+			arr[lines-1][i] = point;
+		}
+
+		return arr;
 	}
 
 	@Override
 	public char[][] draw(String[] input) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		checkInput(input);
+
+		// creates an multi index array with the size inserted by the user (+ the canvas
+		// frame)
+
+		char[][] arr = new char[lines][columns];
+
+		checkInput(input);
+
+		arr = drawX(arr);
+		arr = drawY(arr);
+
+		return arr;
 	}
 
 }
--- src/test/java/drawProgram/CanvasTest.java
+++ src/test/java/drawProgram/CanvasTest.java
@@ -1,14 +1,61 @@
 package drawProgram;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CanvasTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+private Canvas canvas;
+	
+	@Before
+	public void initialization(){
+		canvas = new Canvas();
 	}
+	
+		
+	@Test
+    public void testValid() throws Exception {       
+       String[] input = new String[] {"C", "4", "40"};
+       canvas.draw(input);
+    }
+
+	@Test
+	public void testNegative1() {
+		String[] input = new String[] {"C", "-1", "40"};
+	    Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+	    	canvas.draw(input);
+	    });
+	    Assert.assertEquals("width and height must be positive numbers", exception.getMessage());
+	}
+	
+	@Test
+	public void testNegative2() {
+		String[] input = new String[] {"C", "2", "-5"};
+	    Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+	    	canvas.draw(input);
+	    });
+	    Assert.assertEquals("width and height must be positive numbers", exception.getMessage());
+	}
+	
+	
+	@Test
+	public void testEmpty1() {
+		String[] input = new String[] {"C", "4"};
+	    Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+	    	canvas.draw(input);
+	    });
+	    Assert.assertEquals("please insert a command, width and height value separated by [space]", exception.getMessage());
+	}
+	
+	@Test
+	public void testNonInt() {
+		String[] input = new String[] {"C", "5.2", "6.8"};
+	    Throwable exception = Assert.assertThrows(NumberFormatException.class, () -> {
+	    	canvas.draw(input);
+	    });
+	    Assert.assertEquals("width and height must be integer numbers", exception.getMessage());
+	}
+
 
 }
