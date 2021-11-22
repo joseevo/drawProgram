55177c4814400c72d8d7e1cbe9fe08b8e23ec7de
joseevo <joseevoliveira@gmail.com>
2021-11-22 03:24:44 +0000

    Created Sub Class Rectangle and its Unit testing

--- src/main/java/drawProgram/Rectangle.java
+++ src/main/java/drawProgram/Rectangle.java
@@ -1,29 +1,108 @@
 package drawProgram;
 
-public class Rectangle extends Geometry{
+public class Rectangle extends Geometry {
+
+	private char[][] arr;
+	private int x1;
+	private int y1;
+	private int x2;
+	private int y2;
+	private int inputSize = 5;
+
+	public Rectangle(char[][] arr) {
+		// validates if the canvas has been created
+		if (arr == null) {
+
+			throw new IllegalArgumentException("Please create a Canvas");
+		}
+
+		this.arr = arr;
+
+	}
 
 	@Override
 	public void checkInput(String[] input) {
-		// TODO Auto-generated method stub
-		
+		// validates if the input has size 5 (command, x1,y1,x2,y2)
+		if (input.length != inputSize) {
+
+			throw new IllegalArgumentException("please insert command, x1, y1, x2, y2");
+		}
+
+		// validates if the x1,y1,x2,y2 passed are integer numbers
+		try {
+			// declares the variables with the x1,y1,x2,y2 values
+			x1 = Integer.parseInt(input[1]);
+			y1 = Integer.parseInt(input[2]);
+			x2 = Integer.parseInt(input[3]);
+			y2 = Integer.parseInt(input[4]);
+
+		} catch (Exception ex) {
+
+			throw new NumberFormatException("x1,y1,x2,y2 must be integer numbers");
+
+		}
+
+		// validates if the points inserted are inside the canvas
+		if ((x1 > arr.length - 2) || (x2 > arr.length - 2) || (y1 > arr[1].length - 2) || (y2 > arr[1].length - 2))
+			throw new IllegalArgumentException("Please insert a point inside the canvas scope");
+
+		// validates if the values inserted for the width and height are positive
+		// numbers
+		if (x1 <= 0 || y1 <= 0 || x2 <= 0 || y2 <= 0) {
+
+			throw new IllegalArgumentException("x1,y1,x2,y2 must be positive numbers");
+		}
+
+		// validates if the values inserted don't have equal sides
+		if (x2 - x1 == y2 - y1) {
+
+			throw new IllegalArgumentException("Sides are equal");
+		}
+
+		// validates if the horizontal or the vertical coordinates are different
+		if (x1 == x2 || y1 == y2) {
+
+			throw new IllegalArgumentException(
+					"Please insert different horizontal and vertical coordinates to create a rectangle");
+		}
+
 	}
 
 	@Override
 	public char[][] drawX(char[][] arr) {
-		// TODO Auto-generated method stub
-		return null;
+		char point = 'X';
+
+		for (int i = x1; i <= x2; i++) {
+
+			arr[i][y1] = point;
+			arr[i][y2] = point;
+		}
+
+		return arr;
+
 	}
 
 	@Override
 	public char[][] drawY(char[][] arr) {
-		// TODO Auto-generated method stub
-		return null;
+		char point = 'X';
+
+		for (int i = y1; i <= y2; i++) {
+
+			arr[x1][i] = point;
+			arr[x2][i] = point;
+		}
+
+		return arr;
 	}
 
 	@Override
 	public char[][] draw(String[] input) {
-		// TODO Auto-generated method stub
-		return null;
+		checkInput(input);
+
+		arr = drawX(arr);
+		arr = drawY(arr);
+
+		return arr;
 	}
 
 }
--- src/test/java/drawProgram/RectangleTest.java
+++ src/test/java/drawProgram/RectangleTest.java
@@ -1,14 +1,94 @@
 package drawProgram;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class RectangleTest {
 
+	private Canvas canvas = new Canvas();
+	private String[] inputCanvas = new String[] { "C", "20", "60" };
+	private char[][] arr = canvas.draw(inputCanvas);
+	private Rectangle rectangle = new Rectangle(arr);
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testValid() throws Exception {
+		String[] inputRectangle = new String[] { "R", "1", "1", "2", "3" };
+		rectangle.draw(inputRectangle);
 	}
+
+	@Test
+	public void testNoCanvas() {
+		char[][] arr = null;
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			Rectangle rectangle = new Rectangle(arr);
+		});
+		Assert.assertEquals("Please create a Canvas", exception.getMessage());
+	}
+
+	@Test
+	public void testEmpty1() {
+		String[] inputLine = new String[] { "R", "1", "1", "4" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			rectangle.draw(inputLine);
+		});
+		Assert.assertEquals("please insert command, x1, y1, x2, y2", exception.getMessage());
+	}
+
+	@Test
+	public void testNonInt() {
+		String[] inputLine = new String[] { "R", "4", "10", "10", "5.2" };
+		Throwable exception = Assert.assertThrows(NumberFormatException.class, () -> {
+			rectangle.draw(inputLine);
+		});
+		Assert.assertEquals("x1,y1,x2,y2 must be integer numbers", exception.getMessage());
+	}
+
+	@Test
+	public void testOutScope() {
+		String[] inputLine = new String[] { "R", "4", "10", "6", "70" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			rectangle.draw(inputLine);
+		});
+		Assert.assertEquals("Please insert a point inside the canvas scope", exception.getMessage());
+	}
+
+	@Test
+	public void testNegative1() {
+		String[] inputLine = new String[] { "R", "-4", "10", "4", "20" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			rectangle.draw(inputLine);
+		});
+		Assert.assertEquals("x1,y1,x2,y2 must be positive numbers", exception.getMessage());
+	}
+
+	@Test
+	public void testNegative2() {
+
+		String[] inputLine = new String[] { "R", "4", "-10", "6", "-20" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			rectangle.draw(inputLine);
+		});
+		Assert.assertEquals("x1,y1,x2,y2 must be positive numbers", exception.getMessage());
+	}
+
+	@Test
+	public void testRectangle() {
+		String[] inputLine = new String[] { "R", "1", "1", "3", "3" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			rectangle.draw(inputLine);
+		});
+		Assert.assertEquals("Sides are equal", exception.getMessage());
+	}
+
+	@Test
+	public void testSameCoordinates() {
+		String[] inputLine = new String[] { "R", "1", "2", "1", "5" };
+		Throwable exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+			rectangle.draw(inputLine);
+		});
+		Assert.assertEquals("Please insert different horizontal and vertical coordinates to create a rectangle",
+				exception.getMessage());
+	}
+
 
 }
